e085af8446c253bcd4b3885c25db19fdb7d3a03d
husainleel <73138338+husainleel@users.noreply.github.com>
2021-04-02 20:29:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+var prompt1 = prompt("Player 1");
+var prompt2 = prompt("Player 2");
+
+if (prompt1.length >0 ){
+  document.querySelector(".player-1 p").innerText=prompt1;
+} else {
+
+}
+
+if (prompt2.length >0 ){
+document.querySelector(".player-2 p").innerText=prompt2;
+
+}
+
+
+
+function play() {
+
+
+
+
+
+
+var p1num = (Math.floor(Math.random()*6)) +1 ;
+document.querySelector(".player-1-img").src = "file:///C:/Users/husai/Desktop/Dicee%20Challenge%20-%20Starting%20Files/images/dice"+p1num+".png";
+
+var p2num = (Math.floor(Math.random()*6)) +1 ;
+document.querySelector(".player-2-img").src = "file:///C:/Users/husai/Desktop/Dicee%20Challenge%20-%20Starting%20Files/images/dice"+p2num+".png";
+
+if (p1num>p2num) {
+  document.querySelector(".heading-h1").innerText="🚩 Player 1 wins";
+} else if (p1num<p2num) {
+  document.querySelector(".heading-h1").innerText="Player 2 wins 🚩";
+} else {
+
+}
+}
