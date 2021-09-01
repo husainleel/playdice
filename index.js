d657f8379e3a8afc41e7dea18876f0505e6c7a32
husainleel <73138338+husainleel@users.noreply.github.com>
2021-09-01 23:56:18 +0300

    again

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function play() {
 
 
 var p1num = (Math.floor(Math.random()*6)) +1 ;
-document.querySelector(".player-1-img").src = "images/dice"+p1num+".png";
+document.querySelector(".player-1-img").src = "./images/dice"+p1num+".png";
 
 var p2num = (Math.floor(Math.random()*6)) +1 ;
-document.querySelector(".player-2-img").src = "images/dice"+p2num+".png";
+document.querySelector(".player-2-img").src = "./images/dice"+p2num+".png";
 
 if (p1num>p2num) {
   document.querySelector(".heading-h1").innerText="🚩 Player 1 wins";
